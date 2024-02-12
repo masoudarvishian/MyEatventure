71a98173cdbe7b15ed22858b284c75d4f701e29c
masoudarvishian <masoudarvishian@gmail.com>
2024-02-12 21:49:19 +0100

    Moving the cube

--- Assets/Scripts/Systems/ChefSystems.cs
+++ Assets/Scripts/Systems/ChefSystems.cs
@@ -5,5 +5,6 @@ public class ChefSystems : Feature
     public ChefSystems(Contexts contexts, GameObject chefPrefab)
     {
         Add(new CreateChefSystem(contexts, chefPrefab));
+        Add(new MovingChefSystem(contexts));
     }
 }
---
+++ Assets/Scripts/Systems/MovingChefSystem.cs
@@ -0,0 +1,25 @@
+﻿using Entitas;
+using UnityEngine;
+
+internal class MovingChefSystem : IExecuteSystem
+{
+    private readonly Contexts _contexts;
+    private float speed = 2.0f;
+
+    public MovingChefSystem(Contexts contexts)
+    {
+        _contexts = contexts;
+    }
+
+    public void Execute()
+    {
+        foreach (var e in _contexts.game.GetGroup(GameMatcher.ChefVisual).GetEntities())
+        {
+            var entityTransform = e.chefVisual.gameObject.transform;
+            Vector3 targetPosition = new Vector3(7f, 0, 0);
+            var step = speed * Time.deltaTime;
+            entityTransform.position = Vector3.MoveTowards(entityTransform.position, targetPosition, step);
+            e.position.value = entityTransform.position;
+        }
+    }
+}

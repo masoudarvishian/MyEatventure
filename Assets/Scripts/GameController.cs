2983ad14e7acbc997fcdd2289e7faa6b943b7d39
masoudarvishian <masoudarvishian@gmail.com>
2024-02-12 15:40:52 +0100

    Managed folders

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,22 @@
+using Entitas;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private RootSystems _rootSystems;
+    private Systems _systems;
 
     private void Start()
     {
-        _rootSystems = new RootSystems(Contexts.sharedInstance);
-        _rootSystems.Initialize();
+        _systems = new Feature("My Features").Add(
+            new RootSystems(Contexts.sharedInstance)
+        );
+
+        _systems.Initialize();
     }
 
     private void Update()
     {
-        _rootSystems.Execute();
+        _systems.Execute();
+        _systems.Cleanup();
     }
 }
--- Assets/Scripts/Systems/RootSystems.cs
+++ Assets/Scripts/Systems/RootSystems.cs
@@ -1,8 +1,11 @@
-﻿public class RootSystems : Feature
+﻿using Assets.Scripts.Systems;
+
+public class RootSystems : Feature
 {
     public RootSystems(Contexts contexts)
     {
         Add(new LogHealthSystem(contexts));
         Add(new CreatePlayerSystem(contexts));
+        Add(new CleanUpHealthSystem(contexts));
     }
 }

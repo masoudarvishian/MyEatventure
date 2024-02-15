a769c3986da0c710745b7b9775da9129deb651f2
masoudarvishian <masoudarvishian@gmail.com>
2024-02-15 12:20:02 +0100

    spawn new chef by pressing space

--- Assets/Scripts/Systems/CreateChefSystem.cs
+++ Assets/Scripts/Systems/CreateChefSystem.cs
@@ -2,7 +2,7 @@
 using Entitas.Unity;
 using UnityEngine;
 
-public sealed class CreateChefSystem : IInitializeSystem
+public sealed class CreateChefSystem : IInitializeSystem, IExecuteSystem
 {
     private readonly Contexts _contexts;
     private readonly GameObject _chefPrefab;
@@ -15,10 +15,30 @@ public sealed class CreateChefSystem : IInitializeSystem
 
     public void Initialize()
     {
+        InstantiateAndLinkChef();
+    }
+
+    public void Execute()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InstantiateAndLinkChef();
+        }
+    }
+
+    private void InstantiateAndLinkChef()
+    {
+        var newChefObj = GameObject.Instantiate(_chefPrefab);
+        newChefObj.transform.position = new Vector3(0, 0.5f, 0);
+        CreateAndLinkChefEnitty(newChefObj);
+    }
+
+    private void CreateAndLinkChefEnitty(GameObject chefPrefabObj)
+    {
         var entity = _contexts.game.CreateEntity();
         entity.isChef = true;
-        entity.AddPosition(new Vector3(0, 0, 0));
-        entity.AddVisual(_chefPrefab);
-        _chefPrefab.Link(entity);
+        entity.AddPosition(new Vector3(0, 0.5f, 0));
+        entity.AddVisual(chefPrefabObj);
+        chefPrefabObj.Link(entity);
     }
 }
